ae95103a688fd4ed9816a0b5a79c82a46940e03c
Nihaal Y K <122111345+Nihaalyk@users.noreply.github.com>
2024-10-20 19:29:58 +0530

    generate knowledge graph

---
+++ config.py
@@ -0,0 +1,5 @@
+# Configuration settings for the application
+
+NEON_DB_URI = "postgresql://void_owner:mDtN0peIn3XO@ep-broad-butterfly-a1owz7cl.ap-southeast-1.aws.neon.tech/void?sslmode=require"
+
+GROQ_API_KEY = "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"
